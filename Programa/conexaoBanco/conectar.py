a5d2339d0ced091aa052bb01d5907abcd7e0df5f
BrunoSerpa <serpabruno2005@gmail.com>
2024-10-24 13:11:03 -0300

    Copiei o repositório https://github.com/BrunoSerpa/Loja-Online-Mongo

---
+++ Programa/conexaoBanco/conectar.py
@@ -0,0 +1,11 @@
+from Programa.funcoes.utils.salvarErro import salvarErro
+
+from pymongo import MongoClient
+
+def conectar():
+    try:
+        cliente = MongoClient("linkMongo")
+        return cliente.Loja_Online
+    except Exception as e:
+        salvarErro("Erro ao conectar ao Mongo", e)
+        return None
