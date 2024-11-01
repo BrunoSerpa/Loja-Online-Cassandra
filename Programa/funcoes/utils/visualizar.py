e6ddbb5ee247183587ff229a02404d50f0a6013c
BrunoSerpa <serpabruno2005@gmail.com>
2024-10-31 22:11:03 -0300

    Corrigi as funções de visualizar

--- Programa/funcoes/utils/visualizar.py
+++ Programa/funcoes/utils/visualizar.py
@@ -1,9 +1,10 @@
 from Programa.funcoes.utils.formatar import cep, cnpj, cpf, telefone
 from Programa.funcoes.utils.separar import separador2, separador3
+from Programa.funcoes.crud import buscarPorId
 
 def usuario(usuario, comId = False, comFavoritos = False, comCompras = False, basico = False):
     if comId:
-        print(f"ID: {usuario.get('_id')}")
+        print(f"ID: {usuario.get('id')}")
 
     print(f"Nome: {usuario.get('nome_usuario')}")
     print(f"CPF: {cpf(usuario.get('cpf'))}")
@@ -11,10 +12,10 @@ def usuario(usuario, comId = False, comFavoritos = False, comCompras = False, ba
     if not basico:
         print(f"Telefone: {telefone(usuario.get('telefone_usuario'))}")
         print(f"Email: {usuario.get('email_usuario')}")
-        quantEnderecos = enderecos(usuario.get('enderecos_usuario'))
+        quantEnderecos = enderecos(usuario.get('enderecos')) if 'enderecos' in usuario else 0
 
     if comFavoritos:
-        quantFavoritos = produtos(usuario.get('favoritos'), favoritos = True)
+        quantFavoritos = produtos(usuario.get('favoritos'), favoritos=True)
 
     if comCompras:
         quantCompras = compras(usuario.get('compras'))
@@ -31,7 +32,7 @@ def usuario(usuario, comId = False, comFavoritos = False, comCompras = False, ba
 
 def vendedor(vendedor, comId = False, comProdutos = False, comVendas = False, basico = False):
     if comId:
-        print(f'ID: {vendedor.get("_id")}')
+        print(f'ID: {vendedor.get("id")}')
 
     print(f"Nome: {vendedor.get('nome_vendedor')}")
     print(f"CNPJ: {cnpj(vendedor.get('cnpj'))}")
@@ -39,7 +40,7 @@ def vendedor(vendedor, comId = False, comProdutos = False, comVendas = False, ba
     if not basico:
         print(f"Telefone: {telefone(vendedor.get('telefone_vendedor'))}")
         print(f"Email: {vendedor.get('email_vendedor')}")
-        quantEnderecos = enderecos(vendedor.get('enderecos_vendedor'))
+        quantEnderecos = enderecos(vendedor.get('enderecos'))
 
     if comProdutos:
         quantProdutos = produtos(vendedor.get('produtos'))
@@ -58,45 +59,63 @@ def vendedor(vendedor, comId = False, comProdutos = False, comVendas = False, ba
 
 def compra(compra, comId = False, comVendedor = False, comUsuario = False):
     if comId:
-        print(f'ID: {compra.get("_id")}')
+        print(f'ID: {compra.get("id")}')
 
     print(f'Data da Compra: {compra.get("data_compra")}')
 
     if comUsuario:
         print(separador2)
         print("Cliente:")
-        usuario(compra.get('cliente'), basico = True)
+        cliente_compra = buscarPorId("Usuarios", compra.get('id_cliente'))
+        if cliente_compra:
+            usuario(cliente_compra, basico=True)
+        else:
+            print(f"Id inválido para usuário: {compra.get('id_cliente')}")
 
     print(separador2)
     print("Destinatário:")
-    endereco(compra.get('destinatario'))
-
+    destinatario = buscarPorId("Enderecos", compra.get('destinatario'))
+    if destinatario:
+        endereco(destinatario)
+    else:
+        print(f"Id inválido para endereço: {compra.get('destinatario')}")
+    
     if comVendedor:
         print(separador2)
         print("Vendedor:")
-        vendedor(compra.get('vendedor'), basico = True)
+        vendedor_compra = buscarPorId("Vendedores", compra.get('id_vendedor'))
+        if vendedor_compra:
+            vendedor(vendedor_compra, basico=True)
+        else:
+            print(f"Id inválido para vendedor: {compra.get('id_vendedor')}")
 
-    """    
     print(separador2)
     print("Remetente:")
-    endereco(compra.get('remetente'))
-    """
+    remetente = buscarPorId("Enderecos", compra.get('remetente'))
+    if remetente:
+        endereco(remetente)
+    else:
+        print(f"Id inválido para endereço: {compra.get('remetente')}")
 
     print(separador2)
-    produtos(compra.get("produtos"))
+    produtos(compra.get('produtos'))
 
     print("Total:", compra.get("valor_total"))
 
 def produto(produto, comId=False, comVendedor=False):    
     if comId:
-        print(f'ID: {produto.get("_id")}')
+        print(f'ID: {produto.get("id")}')
 
     print(f"Produto: {produto.get('nome_produto')}")
-    print(f"Valor: R${float(produto.get('valor_produto')):.2f}")
+    print(f"Valor: R${float(produto.get('valor_produto', 0)):.2f}")
 
     if comVendedor:
         print(separador2)
-        vendedor(produto.get('vendedor'), basico=True)
+        vendedor_produto = buscarPorId("Vendedores", produto.get('id_vendedor'))
+        if vendedor_produto:
+            vendedor(vendedor_produto, basico=True)
+        else:
+            print(f"Id inválido para vendedor: {produto.get('id_vendedor')}")
         print(separador2)
 
 def endereco(endereco):
@@ -114,9 +133,13 @@ def enderecos(enderecos):
     else:
         print(separador2)
         print(f"Endereço{'s' if quantidade > 1 else ''}:")
-        for endereco_item in enderecos:
-            print(separador3)
-            endereco(endereco_item)
+        for endereco_id in enderecos:
+            endereco_item = buscarPorId("Enderecos", endereco_id)
+            if endereco_item:
+                print(separador3)
+                endereco(endereco_item)
+            else:
+                print(f"Id inválido para endereço: {endereco_id}")
         print(separador3)
     return quantidade
 
@@ -132,9 +155,13 @@ def produtos(produtos, comId = False, favoritos = False):
         print(separador2)
         print(f"{titulo}{'s' if quantidade > 1 else ''}:")
         
-        for produto_item in produtos:
-            print(separador3)
-            produto(produto_item, comId)
+        for produto_id in produtos:
+            produto_item = buscarPorId("Produtos", produto_id)
+            if produto_item:
+                print(separador3)
+                produto(produto_item, comId)
+            else:
+                print(f"Id inválido para produtos: {produto_id}")
         print(separador3)
     return quantidade
 
@@ -149,8 +176,12 @@ def compras(compras, vendas = False):
     else:
         print(separador2)
         print(f"{titulo}{'s' if quantidade > 1 else ''}:")
-        for compra_item in compras:
-            print(separador3)
-            compra(compra_item, False, not vendas, vendas)
+        for compra_id in compras:
+            compra_item = buscarPorId("Compras", compra_id)
+            if compra_item:
+                print(separador3)
+                compra(compra_item, False, not vendas, vendas)
+            else:
+                print(f"Id inválido para compras: {compra_id}")
         print(separador3)
     return quantidade
