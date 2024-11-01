f5a787a4c1ed66730b0c96f62b1b6e4a3d7c8fbe
BrunoSerpa <serpabruno2005@gmail.com>
2024-10-31 21:57:08 -0300

    Atualizei a função de conectar para se conectar com o Cassandra

--- Programa/conexaoBanco/conectar.py
+++ Programa/conexaoBanco/conectar.py
@@ -1,11 +1,29 @@
-from Programa.funcoes.utils.salvarErro import salvarErro
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
 
-from pymongo import MongoClient
+from Programa.conexaoBanco.configuracao import configuracao, tabelas, indices
+from Programa.funcoes.utils.salvarErro import salvarErro
 
 def conectar():
     try:
-        cliente = MongoClient("linkMongo")
-        return cliente.Loja_Online
+        configNuvem, idCliente, segredoCliente = configuracao()
+        provedorAutenticacao = PlainTextAuthProvider(idCliente, segredoCliente)
+        cluster = Cluster(cloud=configNuvem, auth_provider=provedorAutenticacao)
+        sessao = cluster.connect()
+        sessao.execute("USE loja_online;")
+        # for query in tabelas():
+        #     try:
+        #         sessao.execute(query)
+        #         print(f"Comando executado com sucesso: {query}")
+        #     except Exception as e:
+        #         salvarErro("Erro ao criar tabela", e)
+        # for query in indices():
+        #     try:
+        #         sessao.execute(query)
+        #         print(f"Comando executado com sucesso: {query}")
+        #     except Exception as e:
+        #         salvarErro("Erro ao criar índice", e)
+        return sessao
     except Exception as e:
-        salvarErro("Erro ao conectar ao Mongo", e)
+        salvarErro("Erro ao conectar ao Cassandra", e)
         return None
